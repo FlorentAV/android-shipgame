07bd07ea3d5dd8717f206e9e440f8edd9aedb74a
Florent Avdiu <143091534+FlorentAV@users.noreply.github.com>
2024-12-10 21:33:41 +0100

    Delete app/src/main/java/com/florent/shipgame/models/GameBoard.kt
    No need for gameboard

--- app/src/main/java/com/florent/shipgame/models/GameBoard.kt
+++
@@ -1,8 +0,0 @@
-package com.florent.shipgame.models
-
-data class GameBoard(
-    val size: Int = 10,
-    val ships: MutableList<Ship> = mutableListOf(),
-    val hits: MutableSet<Pair<Int, Int>> = mutableSetOf(),
-    val misses: MutableSet<Pair<Int, Int>> = mutableSetOf()
-)
