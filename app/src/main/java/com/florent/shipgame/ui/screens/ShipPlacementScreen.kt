9a1967f7bbd42ad998a3240a15032250644c8ea1
Florent Avdiu <avfl23ww@student.ju.se>
2024-12-12 01:50:39 +0100

    Updated the UI and some bug fixes, there is still small bugs

--- app/src/main/java/com/florent/shipgame/ui/screens/ShipPlacementScreen.kt
+++ app/src/main/java/com/florent/shipgame/ui/screens/ShipPlacementScreen.kt
@@ -1,5 +1,7 @@
 package com.florent.shipgame.ui.components
+import android.util.Log
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectTapGestures
@@ -17,14 +19,20 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.florent.shipgame.R
 import com.florent.shipgame.logic.GameModel
+import com.florent.shipgame.models.Coordinate
 import com.florent.shipgame.models.Ship
 
 
@@ -38,51 +46,69 @@ fun ShipPlacementScreen(
 ) {
     val context = LocalContext.current
     val players = gameModel.playerMap.collectAsState().value
-    val opponentReady = remember { mutableStateOf(false) }
+    val playerReady = remember { mutableStateOf(false) }
 
-    var currentShipSize by remember { mutableStateOf(4) }
+
+    val savedPlacedShips = rememberSaveable(saver = shipSaver()) { mutableStateListOf<Ship>() }
+    var currentShipSize by remember { mutableIntStateOf(4) }
     var isVertical by remember { mutableStateOf(true) }
-    val placedShips = remember { mutableStateListOf<Ship>() }
-    val gridHighlights = remember { mutableStateListOf<Pair<Int, Int>>() }
+    val placedShips = remember { savedPlacedShips }
+    val gridHighlights = remember { mutableStateListOf<Coordinate>() }
     var showWaitingMessage by remember { mutableStateOf(false) }
+    var isDonePlacing by remember { mutableStateOf(false) }
 
-    // Track the number of ships to place
+
     val shipSizes = listOf(4, 3, 2, 2, 1, 1) // Ship sizes in order, places biggest first
     val totalShips = shipSizes.size
 
     // Check if all ships are placed
     val allShipsPlaced = placedShips.size == totalShips
 
-    val gridSize = 10 // Grid size for board
+    val gridSize = 10
 
-    // Observe opponent's ready status
+    Log.d("ShipPlacementScreen", "Shiplacnment launched")
     LaunchedEffect(gameId) {
-        gameModel.observeGameState(gameId) { gameState ->
-            if (gameState == "ready") {
-                opponentReady.value = true
+        gameModel.observePlayerState(playerId) { playerStatus ->
+            Log.d("ShipPlacementScreen", "Player status changed: $playerStatus")
+            if (playerStatus == "ready") {
+                playerReady.value = true
+            }
+            gameModel.observeGameState(gameId) { gameState ->
+                if (gameState == "in-game" && !isDonePlacing) {
+                    isDonePlacing = true // Flag to prevent multiple navigation
+                    Log.d("ShipPlacementScreen", "Navigating to game screen")
+                    navController.navigate("gameScreen/$gameId/$playerId")
+
+
+                }
             }
         }
     }
 
     // Helper to highlight positions
-    fun calculateHighlightPositions(start: Pair<Int, Int>): List<Pair<Int, Int>> {
-        val (x, y) = start
-        return (0 until currentShipSize).mapNotNull { offset ->
-            val pos = if (isVertical) Pair(x, y + offset) else Pair(x + offset, y)
-            if (pos.first in 0 until gridSize && pos.second in 0 until gridSize) pos else null
+    fun calculateHighlightPositions(start: Coordinate): List<Coordinate> {
+        val positions = mutableListOf<Coordinate>()
+        for (i in 0 until currentShipSize) {
+            val position = if (isVertical) {
+                Coordinate(start.x + i, start.y)
+            } else {
+                Coordinate(start.x, start.y + i)
+            }
+            positions.add(position)
         }
+        return positions
     }
 
     // Validate ship placement
-    fun isPlacementValid(positions: List<Pair<Int, Int>>): Boolean {
+    fun isPlacementValid(positions: List<Coordinate>): Boolean {
         return positions.all { pos ->
-            pos.first in 0 until gridSize && pos.second in 0 until gridSize &&
+            pos.x in 0 until gridSize && pos.y in 0 until gridSize &&
                     placedShips.none { ship -> ship.positions.contains(pos) }
         }
     }
 
     // Place ship
-    fun placeShip(start: Pair<Int, Int>) {
+    fun placeShip(start: Coordinate) {
         if (allShipsPlaced) {
             Toast.makeText(context, "All ships are already placed!", Toast.LENGTH_SHORT).show()
             return
@@ -92,11 +118,12 @@ fun ShipPlacementScreen(
         if (isPlacementValid(positions)) {
             placedShips.add(Ship(size = currentShipSize, positions = positions.toMutableList()))
 
-            // Move to next ship, if there are no left, proceed down
+            // Move to the next ship, if there are none left, show a confirmation message
             if (placedShips.size < totalShips) {
                 currentShipSize = shipSizes[placedShips.size]
             } else {
-                Toast.makeText(context, "All ships placed! Confirm to proceed.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "All ships placed! Confirm to proceed.", Toast.LENGTH_SHORT)
+                    .show()
             }
         } else {
             Toast.makeText(context, "Invalid placement!", Toast.LENGTH_SHORT).show()
@@ -122,42 +149,67 @@ fun ShipPlacementScreen(
                     .padding(padding),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                // Opponent Ready Status
-                if (opponentReady.value) {
-                    Text("Opponent is ready!", color = Color.Green)
-                }
+                // Ship border grid
+                Box(
+                    modifier = Modifier
+                        .aspectRatio(1f)
+                        .size(320.dp)
+                        .border(2.dp, Color.Black)
+                ) {
+                    // Water background image
+                    Image(
+                        painter = painterResource(id = R.drawable.water_background),
+                        contentDescription = "water background",
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop
+                    )
 
-                Spacer(modifier = Modifier.height(16.dp))
+                    // Overlay the grid cells
+                    Column(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        for (row in 0 until gridSize) {
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceEvenly
+                            ) {
+                                for (col in 0 until gridSize) {
+                                    val position = Coordinate(row, col)
+                                    val isHighlighted = gridHighlights.contains(position)
+                                    val isOccupied =
+                                        placedShips.any { ship -> ship.positions.contains(position) }
 
-                // Ship Placement Grid
-                Column {
-                    for (row in 0 until gridSize) {
-                        Row {
-                            for (col in 0 until gridSize) {
-                                val position = Pair(row, col)
-                                val isHighlighted = gridHighlights.contains(position)
-                                val isOccupied = placedShips.any { ship -> ship.positions.contains(position) }
+                                    Box(
+                                        modifier = Modifier
+                                            .weight(1f)
+                                            .aspectRatio(1f)
+                                            .border(
+                                                1.dp,
+                                                Color.Black
+                                            )
+                                            .background(
+                                                when {
+                                                    isOccupied -> Color.Green
+                                                    isHighlighted -> if (isPlacementValid(
+                                                            gridHighlights
+                                                        )
+                                                    ) Color.Gray else Color.Red
 
-                                Box(
-                                    modifier = Modifier
-                                        .size(32.dp)
-                                        .border(1.dp, Color.Black)
-                                        .background(
-                                            when {
-                                                isOccupied -> Color.Green
-                                                isHighlighted -> if (isPlacementValid(gridHighlights)) Color.Gray else Color.Red
-                                                else -> Color.White
-                                            }
-                                        )
-                                        .pointerInput(Unit) {
-                                            detectTapGestures(
-                                                onPress = {
-                                                    gridHighlights.clear()
-                                                    gridHighlights.addAll(calculateHighlightPositions(position))
+                                                    else -> Color.Transparent
                                                 }
                                             )
-                                        }
-                                )
+                                            .pointerInput(Unit) {
+                                                detectTapGestures(
+                                                    onPress = {
+                                                        gridHighlights.clear()
+                                                        gridHighlights.addAll(
+                                                            calculateHighlightPositions(position)
+                                                        )
+                                                    }
+                                                )
+                                            }
+                                    )
+                                }
                             }
                         }
                     }
@@ -167,12 +219,12 @@ fun ShipPlacementScreen(
 
                 // Action Buttons
                 Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+
                     // Rotate Button
                     IconButton(onClick = {
                         isVertical = !isVertical
                         if (gridHighlights.isNotEmpty()) {
-                            // Recalculate highlights with the new orientation
-                            val start = gridHighlights.first() // Take the first highlighted position
+                            val start = gridHighlights.first()
                             gridHighlights.clear()
                             gridHighlights.addAll(calculateHighlightPositions(start))
                         }
@@ -193,14 +245,19 @@ fun ShipPlacementScreen(
 
                 Spacer(modifier = Modifier.height(16.dp))
 
-                // Confirm Button
+                // Confirm Button (enabled when all ships are placed)
                 Button(
                     onClick = {
                         showWaitingMessage = true
                         gameModel.updatePlayerStatus(playerId, "ready") {}
-                        gameModel.transitionGameState(gameId, "ready") {}
+                        gameModel.saveShips(gameId, playerId, placedShips) {}
+                        gameModel.checkIfAllShipsPlaced(gameId) {
+                            if (it) {
+                                gameModel.transitionGameState(gameId, "in-game") {}
+                            }
+                        }
                     },
-                    enabled = allShipsPlaced // Enable only when all ships are placed
+                    enabled = allShipsPlaced
                 ) {
                     Text("Confirm")
                 }
@@ -212,3 +269,8 @@ fun ShipPlacementScreen(
         }
     )
 }
+@Composable
+fun shipSaver() = Saver<MutableList<Ship>, List<Ship>>(
+    save = { it.toList() },
+    restore = { it.toMutableList() }
+)
