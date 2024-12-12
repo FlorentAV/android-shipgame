b7427230c03b747b87eabf20b18604af0092a66b
Florent Avdiu <avfl23ww@student.ju.se>
2024-12-12 14:00:50 +0100

    Cleaned a bit in dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.services)
+
 }
 
 android {
@@ -49,8 +50,6 @@ dependencies {
 
 
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
@@ -59,6 +58,7 @@ dependencies {
     implementation(libs.androidx.material3.adaptive.navigation.suite.android)
     implementation(libs.androidx.ui.text.google.fonts)
     implementation(libs.firebase.firestore.ktx)
+
 
 
     debugImplementation(libs.androidx.ui.tooling)
