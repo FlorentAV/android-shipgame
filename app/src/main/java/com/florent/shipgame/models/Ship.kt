3e88fb4ee68a42043484a9af5c5afcb4d4141e5e
Florent Avdiu <avfl23ww@student.ju.se>
2024-12-12 14:01:10 +0100

    Added isSunk for test

--- app/src/main/java/com/florent/shipgame/models/Ship.kt
+++ app/src/main/java/com/florent/shipgame/models/Ship.kt
@@ -5,5 +5,6 @@ package com.florent.shipgame.models
 
 data class Ship(
     val size: Int = 0,
-    val positions: MutableList<Coordinate> = mutableListOf()
+    val positions: MutableList<Coordinate> = mutableListOf(),
+    var isSunk: Boolean = false
 )
